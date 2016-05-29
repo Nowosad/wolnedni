8cd2a061f50aedba6e1ddaf7e16542474bbd35c5
nowosad <tupiszakaczki@gmail.com>
2016-05-29 21:43:05 +0200

    is.niedzielaa added

--- R/sobotaniedziela.R
+++ R/sobotaniedziela.R
@@ -1,6 +1,5 @@
 #' Funkcja sprawdzajaca czy dana data jest sobota lub niedziela
 #'
-#'
 #' @param data w klasie Date
 #'
 #' @return TRUE/FALSE
@@ -8,24 +7,32 @@
 #' @export
 #'
 #' @examples
-#' data <- as.Date("2012-01-01")
-#' is.sobota(data)
-#' is.niedziela(data)
+#' data1 <- as.Date("2012-01-01")
+#' data2 <- as.Date("2012-01-03")
+#' data3 <- as.Date("2012-01-07")
+#' is.sobota(data1)
+#' is.niedziela(data1)
+#' is.sobota(data2)
+#' is.niedziela(data2)
+#' is.sobota(data3)
+#' is.niedziela(data3)
 
 is.sobota <- function(data){
-        if(class(data)=="Date"){
-        wynik <- format(data,"%u")==6
+        if(inherits(data, 'Date')){
+        wynik <- format(data, "%u") == 6
         return(wynik)
         } else {
-                print("sprawdz klase obiektu!")
+                print("Sprawdz klase obiektu!")
         }
 }
 
+#' @export
+#' @describeIn is.sobota Sobota
 is.niedziela <- function(data){
-        if(class(data)=="Date"){
-                wynik <- format(data,"%u")==7
+        if(inherits(data, 'Date')){
+                wynik <- format(data, "%u") == 7
                 return(wynik)
         } else {
-                print("sprawdz klase obiektu!")
+                print("Sprawdz klase obiektu!")
         }
 }
